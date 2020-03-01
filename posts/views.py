940dd93d834ab747ac086e082de6cc43b087854a
Sam <hello@samsedgeman.co.uk>
2020-03-01 17:12:32 +0000

    Added templates for app, added Heroku and Postgres support for deployment

--- posts/views.py
+++ posts/views.py
@@ -5,16 +5,19 @@ from .forms import BlogPostForm
 
 
 def get_posts(request):
-    """ Create a view that will return a list of Posts that were published prior to 'now'
-    and render them to the 'blogposts.html' template"""
+    """ 
+    Create a view that will return a list of Posts that were published prior to 'now'
+    and render them to the 'blogposts.html' template
+    """
 
-    posts = Post.objects.filter(published_date__lte=timezone.now
-        ()).order_by(-published_date)
+    posts = Post.objects.filter(published_date__lte=timezone.now()
+                                ).order_by('-published_date')
     return render(request, "blogposts.html", {'posts': posts})
 
 
 def post_detail(request, pk):
-    """ Create a view that returns a single 
+    """
+    Create a view that returns a single 
     post object based on the post ID (pk) and
     render it to the 'postdetail.html' template.
     Or return a 404 error if the post is not found
@@ -24,14 +27,17 @@ def post_detail(request, pk):
     post.save()
     return render(request, "postdetail.html", {'post': post})
 
+
 def create_or_edit_post(request, pk=None):
-    """ Create a view that allows us to create
+    """
+    Create a view that allows us to create
     or edit a post depending if the Post ID
-    is null or not"""
+    is null or not
+    """
 
     post = get_object_or_404(Post, pk=pk) if pk else None
     if request.method == "POST":
-        form = BlogPostForm(request.POST, request.Files, instance=post)
+        form = BlogPostForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
             post = form.save()
             return redirect(post_detail, post.pk)
